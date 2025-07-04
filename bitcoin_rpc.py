80f8c837f765b0d69db764dc4893d326d8ee8e2b
xstoicunicornx <xstoicunicornx@users.noreply.github.com>
2025-07-04 05:37:22 +0000

    might update rpc to load RPC_PASSWORD based on cookie file path

--- bitcoin_rpc.py
+++ bitcoin_rpc.py
@@ -2,10 +2,13 @@ import json
 import requests
 
 RPC_USER = '__cookie__'
-RPC_PASSWORD = 'set your own password or look at the contents of the .cookie file'
+RPC_COOKIE = '/data/bitcoin/.cookie'
 RPC_URL = f'http://127.0.0.1:8332/'
 
 HEADERS = {'content-type': 'application/json'}
+
+with open(RPC_COOKIE, 'r') as file:
+    cookie = file.read().rstrip()
 
 def rpc_call(method, params=None):
     payload = json.dumps({
@@ -14,6 +17,6 @@ def rpc_call(method, params=None):
         "jsonrpc": "2.0",
         "id": 0,
     })
-    response = requests.post(RPC_URL, headers=HEADERS, data=payload, auth=(RPC_USER, RPC_PASSWORD))
+    response = requests.post(RPC_URL, headers=HEADERS, data=payload, auth=(RPC_USER, cookie))
     response.raise_for_status()
     return response.json()['result']
